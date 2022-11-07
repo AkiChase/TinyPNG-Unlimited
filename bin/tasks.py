4b021e1e76d4626dffbf4d4d7b1bcd0a3ca75fa9
如初 <1003019131@qq.com>
2022-11-07 23:40:25 +0800

    Add tasks.py template

---
+++ bin/tasks.py
@@ -0,0 +1,9 @@
+# ======================================================
+# Uss file_tasks or dir_tasks to create the tasks file.
+# Using both of them or one of them is ok.
+# By the way, the images will cover themselves.
+
+# example:
+
+# file_tasks = ['D:\\1.jpg', 'D:\\2.jpg']
+# dir_tasks = ['D:\\dir1', 'D:\\dir2']
